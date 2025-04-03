2a7e1e8e82b60b08fdf96db028cf049ebcb48150
rchevalier619 <rchevalier@email.neit.edu>
2025-04-03 09:07:23 -0400

    04/03 Class added

---
+++ week2/js/script.js
@@ -0,0 +1,5 @@
+// Example of Javascript file
+
+var name = prompt("What is your name?");
+document.getElementById("greeting").innerHTML = `Hello ${name} how are you today?`
+document.getElementById("greeting").style.color = "black"
